77db0aecc52397b29ace0b833c97c54f55fd6a38
sangheon-kim <ksj83671@gmail.com>
2025-07-17 15:35:21 +0900

    [김상헌] Component Scan 전 JavaOqnlyAppConfig 작업

--- server-basic/src/main/java/core/base/JavaOnlyAppConfig.java
+++ server-basic/src/main/java/core/base/JavaOnlyAppConfig.java
@@ -15,35 +15,28 @@ import javax.sql.DataSource;
 
 import static core.base.javaonly.connection.ConnectionConst.*;
 
-@Configuration
 public class JavaOnlyAppConfig {
 
-    @Bean
     public DataSource dataSource() {
         return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
     }
 
-    @Bean
     public EmployeeRepository employeeRepository() {
         return new JDBCEmployeeRepository(dataSource());
     }
 
-    @Bean
     public PayrollRepository payrollRepository() {
         return new JDBCPayrollRepository(dataSource());
     }
 
-    @Bean
     public IncentivePolicy incentivePolicy() {
         return new SalaryRatioIncentivePolicy(JobLevel.Manager, 0.1);
     }
 
-    @Bean
     public PayrollService payrollService() {
         return new PayrollServiceImpl(incentivePolicy(), employeeRepository(), payrollRepository());
     }
 
-    @Bean
     public EmployeeService employeeService() {
         return new EmployeeServiceImpl(employeeRepository());
     }
