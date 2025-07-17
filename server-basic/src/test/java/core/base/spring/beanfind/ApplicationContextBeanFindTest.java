f0a0a2ff44d2d78f64b6bc6a8b371317a2988328
sangheon-kim <ksj83671@gmail.com>
2025-07-17 13:46:22 +0900

    [김상헌] 컴포넌트 스캔 작업 전 커밋

--- server-basic/src/test/java/core/base/spring/beanfind/ApplicationContextBeanFindTest.java
+++ server-basic/src/test/java/core/base/spring/beanfind/ApplicationContextBeanFindTest.java
@@ -22,7 +22,16 @@ public class ApplicationContextBeanFindTest {
     @Test
     @DisplayName("이름 없이 타입만으로 조회 (상속된다 상위타입 조회시 하위 타입들의 빈들 다 조회)")
     void findBeanByType() {
+        // 스프링 빈에 부모타입으로 조회하면 자식 타입도 조회된다. Object 타입으로 조회하면 모든 스프링 빈을 조회한다.
         EmployeeService employeeService = ac.getBean(EmployeeService.class);
+        Assertions.assertThat(employeeService).isInstanceOf(EmployeeServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        // 구체타입으로 조회하면 변경시 유연성이 떨어진다.
+        EmployeeServiceImpl employeeService = ac.getBean(EmployeeServiceImpl.class);
         Assertions.assertThat(employeeService).isInstanceOf(EmployeeServiceImpl.class);
     }
 }
